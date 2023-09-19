6afc12b5ae19a65f8d1b49a9727fd7b8d5659025
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-19 12:38:38 +0300

    Create Routes'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'messages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :messages, only: [:index]
   namespace :api do
     get 'random_greeting', to: 'greetings#random'
   end
